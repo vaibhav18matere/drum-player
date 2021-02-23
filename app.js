b7df037d4061a62838c42fccb127ba093b934668
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-02-23 11:38:23 +0530

    keyboard event added

--- app.js
+++ app.js
@@ -1,51 +1,79 @@
 //adding event listener using querySelector
 
 let numberOfButtons = document.querySelectorAll("button").length;
+console.log(numberOfButtons);
 
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+})
 
-for (i = 0; i <= numberOfButtons; i++) {
+for (i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener('click', function () {
         let buttonInner = this.innerHTML
+        // console.log(buttonInner);
 
-switch (buttonInner) {
-    case 'w':
-        var audi1 = new Audio("sounds/tom-1.mp3")
-        audi1.play();
-        break;
+        makeSound(buttonInner);
 
-    case 'e':
-        var audi2 = new Audio("sounds/tom-2.mp3")
-        audi2.play();
-        break;
-    case 'r':
-        var audi3 = new Audio("sounds/tom-3.mp3")
-        audi3.play();
-        break;
-    case 't':
-        var audi4 = new Audio("sounds/tom-4.mp3")
-        audi4.play();
-        break;
-    case 'y':
-        var audi5= new Audio("sounds/crash.mp3")
-        audi5.play();
-        break;
-
-    case 'o':
-        var audi6 = new Audio("sounds/kick-bass.mp3")
-        audi6.play();
-        break;
-    case 'u':
-        var audi7 = new Audio("sounds/snare.mp3")
-        audi7.play();
-        break;
-
-    default:console.log("cant play");
-        break;
-}
 
     });
 
 }
+
+
+
+
+
+
+function makeSound(key){
+    switch (key) {
+        case 'w':
+            var audi1 = new Audio("sounds/tom-1.mp3")
+            audi1.play();
+            break;
+    
+        case 'e':
+            var audi2 = new Audio("sounds/tom-2.mp3")
+            audi2.play();
+            break;
+        case 'r':
+            var audi3 = new Audio("sounds/tom-3.mp3")
+            audi3.play();
+            break;
+        case 't':
+            var audi4 = new Audio("sounds/tom-4.mp3")
+            audi4.play();
+            break;
+        case 'y':
+            var audi5= new Audio("sounds/crash.mp3")
+            audi5.play();
+            break;
+    
+        case 'o':
+            var audi6 = new Audio("sounds/kick-bass.mp3")
+            audi6.play();
+            break;
+        case 'u':
+            var audi7 = new Audio("sounds/snare.mp3")
+            audi7.play();
+            break;
+    
+        default:console.log("sounds/crash.mp3");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let soundPlay=document.getElementsByClassName("drum");
 // let soundList=Math.floor(Math.random()*4+1);
 // let sounndSource="tom"+soundList+".mp3";
