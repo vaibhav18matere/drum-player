280839b73ee05e73d60a11a11cc91d352d1b7f6b
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-02-23 10:46:43 +0530

    sound and button connected

--- app.js
+++ app.js
@@ -5,9 +5,44 @@ let numberOfButtons = document.querySelectorAll("button").length;
 
 for (i = 0; i <= numberOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener('click', function () {
-        
-        var audi=new Audio("sounds/tom-1.mp3")
-        audi.play();
+        let buttonInner = this.innerHTML
+
+switch (buttonInner) {
+    case 'w':
+        var audi1 = new Audio("sounds/tom-1.mp3")
+        audi1.play();
+        break;
+
+    case 'e':
+        var audi2 = new Audio("sounds/tom-2.mp3")
+        audi2.play();
+        break;
+    case 'r':
+        var audi3 = new Audio("sounds/tom-3.mp3")
+        audi3.play();
+        break;
+    case 't':
+        var audi4 = new Audio("sounds/tom-4.mp3")
+        audi4.play();
+        break;
+    case 'y':
+        var audi5= new Audio("sounds/crash.mp3")
+        audi5.play();
+        break;
+
+    case 'o':
+        var audi6 = new Audio("sounds/kick-bass.mp3")
+        audi6.play();
+        break;
+    case 'u':
+        var audi7 = new Audio("sounds/snare.mp3")
+        audi7.play();
+        break;
+
+    default:console.log("cant play");
+        break;
+}
+
     });
 
 }
@@ -15,7 +50,6 @@ for (i = 0; i <= numberOfButtons; i++) {
 // let soundList=Math.floor(Math.random()*4+1);
 // let sounndSource="tom"+soundList+".mp3";
 // let sound="sound/"+sounndSource;
-
 
 
 
@@ -38,5 +72,5 @@ for (i = 0; i <= numberOfButtons; i++) {
 //     document.getElementsByClassName('drum')[i].addEventListener('click', function(){
 //         alert('clicked');
 //     })
-    
+
 // }
